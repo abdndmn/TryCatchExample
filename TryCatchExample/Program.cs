8d1faa284f3bef1410d98aa4ea8edb1b4d3b8f69
abdndmn <abdndmn@hotmail.com>
2025-01-21 02:32:57 +0300

    Add project files.

---
+++ TryCatchExample/Program.cs
@@ -0,0 +1,10 @@
+﻿try
+{
+    Console.WriteLine("Lutfen bir sayı giriniz:");
+    int i = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Girdiğiniz sayının karesi: {i * i}");
+}
+catch(Exception ex)
+{
+    Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+}
